0a757286778b2ff2c45d20b8d6674f0b4c6ac08b
tolulopeoluyadi <109180539+tolulopeoluyadi@users.noreply.github.com>
2023-05-01 05:17:49 -0400

    new changes

---
+++ password_strength/__init__.py
@@ -0,0 +1,4 @@
+from passwords import password_strength
+
+result = password_strength("my_password")
+print(result)
--- password_strength/setup.py
+++ password_strength/setup.py
@@ -4,12 +4,12 @@ setup(
     name='password_strength',
     version='0.1',
     description='Check the strength of a password',
-    py_modules=['password_strength'],
+    py_modules=['password_strength', 'passwords'],
     entry_points={
-        'console_scripts': [
-            'password_strength=password_strength:password_strength'
-        ]
+        'console_scripts': ['password_strength=password_strength:password_strength']
     },
+    install_requires=['password_strength'],
+    
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
