ad19b32449fd99c3c4b1c60a70bbb32f86635717
tolulopeoluyadi <109180539+tolulopeoluyadi@users.noreply.github.com>
2023-05-01 04:28:53 -0400

    rough draft of code

---
+++ passwords.py
@@ -0,0 +1,25 @@
+def password_strength(password):
+    # Check the length of the password
+    if len(password) < 8:
+        return "weak"
+
+    # Check for uppercase, lowercase, digits and special characters
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+    has_special = False
+
+    for char in password:
+        if char.isupper():
+            has_uppercase = True
+        elif char.islower():
+            has_lowercase = True
+        elif char.isdigit():
+            has_digit = True
+        else:
+            has_special = True
+
+    if has_uppercase and has_lowercase and has_digit and has_special:
+        return "strong"
+    else:
+        return "medium"
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='password_strength',
+    version='0.1',
+    description='Check the strength of a password',
+    py_modules=['password_strength'],
+    entry_points={
+        'console_scripts': [
+            'password_strength=password_strength:password_strength'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
